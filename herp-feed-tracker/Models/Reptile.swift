e683c274d0dd067c4eeb36017bfa5e2bf4b4764a
Jacob Stanger <jacobcstanger@gmail.com>
2020-12-05 11:59:29 -0700

    add reptile view model

--- herp-feed-tracker/Models/Reptile.swift
+++ herp-feed-tracker/Models/Reptile.swift
@@ -9,14 +9,27 @@ import Foundation
 
 class Reptile: Identifiable, ObservableObject {
   let id = UUID()
-  var name: String = "Rascal"
-  var icon: String = "default_reptile"
-  var description: String = "some cool reptile I own"
-  var type: Type = Type.snake
-  var birthDate: Date = Date()
+  @Published var name: String = "Rascal"
+  @Published var icon: String = "default_reptile"
+  @Published var description: String = "some cool reptile I own"
+  @Published var type: Type = Type.snake
+  @Published var birthDate: Date = Date()
     
   init(name: String ) {
     self.name = name
   }
   
 }
+
+
+//class UserViewModel: ObservableObject {
+//    // Input
+//    @Published var username = ""
+//    @Published var password = ""
+//    @Published var passwordAgain = ""
+//
+//    // Output
+//    @Published var isValid = false
+//}
+
+//@ObservedObject private var userViewModel = UserViewModel()

175cee3cb9929649c42f01008026b001d50d7241
Jacob Stanger <jacobcstanger@gmail.com>
2021-01-11 10:08:29 -0700

    add settings

--- herp-feed-tracker/Views/SettingsView.swift
+++ herp-feed-tracker/Views/SettingsView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+var someSettings = ["General", "Dark Mode", "Other setting"]
+
+
 struct SettingsView: View {
     var body: some View {
-        Text("This is the Settings view")
+      VStack {
+        Text("Settings").font(.title)
+        HStack {
+          List(someSettings, id: \.self) { data in
+            settingsLine(settingText: data)
+          }
+        }
+      }
     }
+}
+
+struct settingsLine: View {
+  @State private var status = true
+  var settingText = ""
+  
+  var body: some View {
+    VStack {
+      HStack {
+        Toggle(isOn: $status) {
+          Text(settingText)
+        }
+
+      }
+    }
+  }
 }
 
 struct SettingsView_Previews: PreviewProvider {
